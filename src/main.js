dc85e4d250f9dc9edfe1c1884d93f54ae29afd42
G-hanyanan <1209848443@qq.com>
2020-03-01 17:03:49 +0800

    项目初始化，资源准备

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUi from 'element-ui' // 引入Element-ui框架
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import '@/styles/index.less' // 引入初始化less文件
 
 Vue.config.productionTip = false
 Vue.use(ElementUi) // 全局注册ElementUi
