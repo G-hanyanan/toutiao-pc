df4a640157be18408cff37778c8c6d134a46813a
G-hanyanan <1209848443@qq.com>
2020-03-01 16:38:32 +0800

    安装ElementUi框架

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui' // 引入Element-ui框架
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUi) // 全局注册ElementUi
 new Vue({
   router,
   render: h => h(App)
