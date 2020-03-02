7e5084ccb0304cce4e8c69444038b34b37b3ff7e
G-hanyanan <1209848443@qq.com>
2020-03-02 19:03:58 +0800

    登录页搭建完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>登录页面</div>
+  <div class="bg">
+    <el-card class="card">
+      <div class="logo">
+        <img style="height:40px" src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form>
+        <el-form-item>
+          <el-input style="marginTop:40px" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input style="width:60%"  placeholder="请输入验证码"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>您是否已阅读协议</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
+<style lang="less" scoped>
+.bg {
+  background-image: url("../../assets/img/login_bg.jpg");
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  .card {
+    width: 450px;
+    height: 350px;
+    background-color: rgba(0, 0, 0, 0);
+    .logo {
+      text-align: center;
+    }
+  }
+}
 </style>
