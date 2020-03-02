2b1f8001be7f8c7f382adcf6f2601ff3639481c8
G-hanyanan <1209848443@qq.com>
2020-03-03 00:17:51 +0800

    主页基本布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>主页</div>
+<!-- 外层容器 所有的子容器都需要放在container内 -->
+  <el-container>
+    <!-- 侧边栏容器 -->
+    <el-aside>
+      <h1>侧边栏</h1>
+    </el-aside>
+    <!-- 右侧容器 上下布局 用外层容器包裹 -->
+    <el-container>
+      <!-- 头部容器 -->
+      <el-header>
+        <h1>头部区域</h1>
+      </el-header>
+      <!-- 主要内容区域 -->
+      <el-main>
+        <!-- 这里放的是二级路由容器 -->
+        <h1>主要内容区域</h1>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
