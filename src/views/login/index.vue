bace859b59f2bf22401fae6598a91b073cf2a4a7
G-hanyanan <1209848443@qq.com>
2020-03-02 21:04:46 +0800

    登录页面 表单数据校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="bg">
     <el-card class="card">
+      <!-- 头部logo -->
       <div class="logo">
         <img style="height:40px" src="../../assets/img/logo_index.png" alt />
       </div>
-      <el-form>
-        <el-form-item>
-          <el-input style="marginTop:40px" placeholder="请输入手机号"></el-input>
+      <!-- 表单 -->
+      <el-form :model="loginForm" :rules="loginRules" ref="loginForm">
+        <!-- 表单容器 -->
+        <el-form-item prop="mobile">
+          <!-- 手机号输入框 -->
+          <el-input v-model="loginForm.mobile" style="marginTop:40px" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:60%"  placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <!-- 验证码输入框 -->
+          <el-input v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
+          <!-- 发送验证码按钮 -->
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>您是否已阅读协议</el-checkbox>
+        <el-form-item prop="checked">
+          <!-- 是否同意协议 -->
+          <el-checkbox v-model="loginForm.checked">您是否已阅读协议</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%" type="primary">登录</el-button>
+          <!-- 登录按钮 -->
+          <el-button style="width:100%" type="primary" @click='submitLoginForm'>登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -24,7 +32,55 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否同意协议
+      },
+      loginRules: {
+        // 手机号输入框规则
+        mobile: [{
+          required: true,
+          message: '请输入您的手机号'
+        }, {
+          pattern: /^1[3-9]\d{9}$/, // 正则表达式
+          message: '您的手机号格式不正确'
+        }],
+        // 验证码框规则
+        code: [{
+          required: true,
+          message: '请输入您的验证码'
+        }, {
+          pattern: /^\d{6}$/, // 正则表达式
+          message: '验证码为6位数字'
+        }],
+        checked: [{
+          validator: function (rule, value, callback) {
+            value ? callback() : callback(new Error('您必须同意协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    submitLoginForm () {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          alert('校验通过')
+        } else {
+          alert('校验未通过')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
