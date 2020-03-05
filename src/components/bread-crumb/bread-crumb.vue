6cf7e7b2d470e9ac06453e641f92c4c8d2efdf19
G-hanyanan <1209848443@qq.com>
2020-03-05 20:14:08 +0800

    封装面包屑组件

---
+++ src/components/bread-crumb/bread-crumb.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>
+          <slot name=''></slot>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@
 
 import layoutAside from '@/components/home/layout-aside.vue'
 import layoutHeader from '@/components/home/layout-header.vue'
+import breadCrumb from '@/components/bread-crumb'
 
 export default {
   install: function (Vue) {
-    Vue.component('layout-aside', layoutAside)
-    Vue.component('layout-header', layoutHeader)
+    Vue.component('layout-aside', layoutAside) // 侧边导航栏组件
+    Vue.component('layout-header', layoutHeader) // 头部组件
+    Vue.component('bread-crumb', breadCrumb) // 封装面包屑组件
   }
 }
