195ec1778f1f2d6157f16aa8324d5e2e657d7415
G-hanyanan <1209848443@qq.com>
2020-03-05 23:22:48 +0800

    打开关闭评论功能

--- src/components/bread-crumb/index.vue
+++ src/components/bread-crumb/index.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
-    <el-breadcrumb separator-class="el-icon-arrow-right">
+  <el-card>
+    <el-breadcrumb slot="header" separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>
         <slot name="title"></slot>
       </el-breadcrumb-item>
     </el-breadcrumb>
-    <el-table :data='list'>
+    <el-table :data="list">
       <el-table-column prop="title" label="标题" width="600"></el-table-column>
-      <el-table-column :formatter='caozuo' prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
       <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
       <el-table-column label="操作">
-        <el-button size="small" type="text">修改</el-button>
-        <el-button size="small" type="text">关闭评论</el-button>
+        <!-- el-table-column组件 scoped slot 可以获取到 row, column, $index 和 store -->
+        <template slot-scope='obj'>
+          <el-button size="small" type="text">修改</el-button>
+          <!-- openOrClose需要将本行数据传进去 -->
+          <el-button @click='openOrClose(obj.row)' size="small" type="text">{{obj.row.comment_status ? '关闭' : '打开'}}评论</el-button>
+        </template>
       </el-table-column>
     </el-table>
-  </div>
+  </el-card>
 </template>
 
 <script>
@@ -36,8 +40,44 @@ export default {
         this.list = res.data.results
       })
     },
-    caozuo (row, column, cellValue, index) {
+    // 处理评论状态布尔值不显示
+    formatterBool (row, column, cellValue, index) {
       return cellValue ? '正常' : '关闭'
+    },
+    // 打开、关闭评论
+    openOrClose (row) {
+      // console.log(row)
+      // alert(row.comment_status)
+      const status = row.comment_status ? '打开' : '关闭'
+      // 调用$confirm方法即可打开消息提示
+      this.$confirm(`${status}评论, 是否继续?`, '提示').then(() => {
+        this.$axios({
+          url: '/comments/status',
+          method: 'put', // 请求类型
+          // query参数
+          params: {
+            article_id: row.id // 文章id
+          },
+          //  body参数
+          data: {
+            allow_comment: !row.comment_status // 当前状态是关闭，就打开评论 取反
+          }
+        }).then(() => {
+          this.$message({
+            showClose: true,
+            message: `${status}评论成功`,
+            type: 'success'
+          })
+        }).catch(() => {
+          this.$message({
+            showClose: true,
+            message: `${status}评论失败`,
+            type: 'error'
+          })
+        })
+      }).catch(() => {
+        return false
+      })
     }
   },
   created () {
