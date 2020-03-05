ad664cfe09ba26521be35a20968b790bc83cda10
G-hanyanan <1209848443@qq.com>
2020-03-05 21:59:38 +0800

    评论页基本结构

--- src/components/bread-crumb/index.vue
+++ src/components/bread-crumb/index.vue
@@ -6,11 +6,26 @@
         <slot name="title"></slot>
       </el-breadcrumb-item>
     </el-breadcrumb>
+    <template>
+      <el-table>
+        <el-table-column prop="id" label="标题" width="600"></el-table-column>
+        <el-table-column prop="name" label="评论状态"></el-table-column>
+        <el-table-column prop="date" label="总评论数"></el-table-column>
+        <el-table-column prop="date" label="粉丝评论数"></el-table-column>
+        <el-table-column prop="date" label="操作"></el-table-column>
+      </el-table>
+    </template>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      list: [{ id: 1, name: '张三', date: '2020年3月5日21:54:48' }]
+    }
+  }
+}
 </script>
 
 <style>
