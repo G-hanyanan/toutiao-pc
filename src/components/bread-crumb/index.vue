0ce1a272eeb5ca073511b3eadf9aca935bc5f389
G-hanyanan <1209848443@qq.com>
2020-03-05 22:33:27 +0800

    评论加载参数及状态异常

--- src/components/bread-crumb/index.vue
+++ src/components/bread-crumb/index.vue
@@ -6,15 +6,16 @@
         <slot name="title"></slot>
       </el-breadcrumb-item>
     </el-breadcrumb>
-    <template>
-      <el-table>
-        <el-table-column prop="id" label="标题" width="600"></el-table-column>
-        <el-table-column prop="name" label="评论状态"></el-table-column>
-        <el-table-column prop="date" label="总评论数"></el-table-column>
-        <el-table-column prop="date" label="粉丝评论数"></el-table-column>
-        <el-table-column prop="date" label="操作"></el-table-column>
-      </el-table>
-    </template>
+    <el-table :data='list'>
+      <el-table-column prop="title" label="标题" width="600"></el-table-column>
+      <el-table-column :formatter='caozuo' prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作">
+        <el-button size="small" type="text">修改</el-button>
+        <el-button size="small" type="text">关闭评论</el-button>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -22,8 +23,25 @@
 export default {
   data () {
     return {
-      list: [{ id: 1, name: '张三', date: '2020年3月5日21:54:48' }]
+      list: []
     }
+  },
+  methods: {
+    // 加载评论数据
+    getComment () {
+      this.$axios({
+        url: '/articles', // 请求地址
+        params: { response_type: 'comment' } // 请求get参数
+      }).then(res => {
+        this.list = res.data.results
+      })
+    },
+    caozuo (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
   }
 }
 </script>
