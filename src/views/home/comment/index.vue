577444890578e4a592e1494908e472f700ff8630
G-hanyanan <1209848443@qq.com>
2020-03-07 15:45:18 +0800

    评论页 分页功能

--- src/views/home/comment/index.vue
+++ src/views/home/comment/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading"
+    element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)">
     <!-- 面包屑 -->
     <bread-crumb slot="header">
       <template slot="title">评论管理</template>
@@ -23,6 +26,16 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-row class="pagination" type="flex" align="middle" justify="center">
+      <!-- total总条数 layout 分页跳配置  -->
+      <el-pagination
+        @current-change="currentChange"
+        :current-page="pages.currentPage"
+        background
+        layout="prev, pager, next"
+        :total="pages.totalCount"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -30,17 +43,27 @@
 export default {
   data () {
     return {
-      list: []
+      pages: {
+        currentPage: 1,
+        totalCount: 0
+      },
+      list: [],
+      loading: false
     }
   },
   methods: {
     // 加载评论数据
-    getComment () {
+    getComment (page) {
+      // 发送请求开启遮罩层
+      this.loading = true
       this.$axios({
         url: '/articles', // 请求地址
-        params: { response_type: 'comment' } // 请求get参数
+        params: { response_type: 'comment', page } // 请求get参数
       }).then(res => {
         this.list = res.data.results
+        this.pages.totalCount = res.data.total_count
+        // 请求成功关闭遮罩层
+        this.loading = false
       })
     },
     // 处理评论状态布尔值不显示
@@ -53,42 +76,53 @@ export default {
       // alert(row.comment_status)
       const status = row.comment_status ? '关闭' : '打开'
       // 调用$confirm方法即可打开消息提示
-      this.$confirm(`${status}评论, 是否继续?`, '提示').then(() => {
-        this.$axios({
-          url: '/comments/status',
-          method: 'put', // 请求类型
-          // query参数
-          params: {
-            article_id: row.id.toString() // 文章id  大数字BigNum要转换成字符串格式
-          },
-          //  body参数
-          data: {
-            allow_comment: !row.comment_status // 当前状态是关闭，就打开评论 取反
-          }
-        }).then(() => {
-          this.$message({
-            showClose: true,
-            message: `${status}评论成功`,
-            type: 'success'
+      this.$confirm(`${status}评论, 是否继续?`, '提示')
+        .then(() => {
+          this.$axios({
+            url: '/comments/status',
+            method: 'put', // 请求类型
+            // query参数
+            params: {
+              article_id: row.id.toString() // 文章id  大数字BigNum要转换成字符串格式
+            },
+            //  body参数
+            data: {
+              allow_comment: !row.comment_status // 当前状态是关闭，就打开评论 取反
+            }
           })
-          this.getComment()
-        }).catch(() => {
-          this.$message({
-            showClose: true,
-            message: `${status}评论失败`,
-            type: 'error'
-          })
+            .then(() => {
+              this.$message({
+                showClose: true,
+                message: `${status}评论成功`,
+                type: 'success'
+              })
+              this.getComment()
+            })
+            .catch(() => {
+              this.$message({
+                showClose: true,
+                message: `${status}评论失败`,
+                type: 'error'
+              })
+            })
         })
-      }).catch(() => {
-        return false
-      })
+        .catch(() => {
+          return false
+        })
+    },
+    currentChange (val) {
+      this.pages.currentPage = val
+      this.getComment(this.pages.currentPage)
     }
   },
   created () {
-    this.getComment()
+    this.getComment(this.pages.currentPage)
   }
 }
 </script>
 
-<style>
+<style lang="less" scopoed>
+.pagination {
+  height: 100px;
+}
 </style>
